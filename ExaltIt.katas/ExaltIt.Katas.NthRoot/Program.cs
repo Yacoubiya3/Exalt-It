43e5595109eea0faaef229ca62fd41a47b06ced5
Yakoubi Mohamed <ya3yacoubi@gmail.com>
2022-03-28 15:41:14 +0200

    katas optimisation
    katas optimisation

---
+++ ExaltIt.katas/ExaltIt.Katas.NthRoot/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ExaltIt.Katas.NthRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nth Root Calculator");
+
+            ShowMenu();
+        }
+
+        static void ShowMenu()
+        {
+            double number = 0;
+            while (number == 0)
+            {
+                Console.WriteLine("Please enter a positive real number for nth calculation");
+
+                if (double.TryParse(Console.ReadLine(), out number) && number > 0)
+                {
+                    Console.WriteLine("Thank you");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error. Please try again");
+                }
+            }
+
+            int root = 0;
+            while (root == 0)
+            {
+                Console.WriteLine("Please enter a positive Natural number as a root");
+
+                if (int.TryParse(Console.ReadLine(), out root) && root > 0)
+                {
+                    Console.WriteLine("Thank you");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error. Please try again");
+                }
+            }
+
+            Console.WriteLine("Calculation in progress...");
+            double? rst = GetNthRoot(number, root);
+
+            if (rst.HasValue)
+            {
+                Console.WriteLine($"Result : {rst.Value}");
+                Console.WriteLine("Please try again");
+                ShowMenu();
+            }
+            else
+            {
+                Console.WriteLine("Calculation Error. Please try again");
+                ShowMenu();
+            }
+        }
+
+        static double? GetNthRoot(double number, int n)
+        {
+            if (number <= 0 || n <= 0)
+            {
+                return null;
+            }
+
+            var task = new Task<double>(() => GetNthRootNewtonMethod(number, n, .000000000000001),
+                    TaskCreationOptions.LongRunning);
+            task.Start();
+
+            return task.Result;
+        }
+
+        static double GetNthRootNewtonMethod(double number, int root, double p)
+        {
+            double rst = number;
+            double temp = number / root;
+
+            while (Abs(rst - temp) > p)
+            {
+                temp = rst;
+                rst = (1 / (double)root) * (((root - 1) * rst) + (number / Power(rst, root - 1)));
+            }
+            return rst;
+        }
+
+
+        static double Power(double number, int n)
+        {
+            double rst = 1.0;
+
+            Parallel.For(1, n + 1,
+                   index =>
+                   {
+                       rst = rst * number;
+                   });
+
+            return rst;
+        }
+
+        static double Abs(double number)
+        {
+            if (number < 0) number = -number;
+            return number;
+        }
+    }
+}
---
+++ ExaltIt.katas/ExaltIt.katas.SameHashCode/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExaltIt.katas.SameHashCode
+{
+    class Program
+    {
+        static List<StringHolder> LsMatchHolder = new List<StringHolder>();
+        static Dictionary<int, string> LsStringMap = new Dictionary<int, string>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generation of the 3 strings with the same hashcode");
+            Console.WriteLine($"{DateTime.Now.ToString("T")} \t Init Tasks");
+            Task<List<string>>[] lsTasks = new Task<List<string>>[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                int taskIndex = i + 1;
+
+                Console.WriteLine($"{DateTime.Now.ToString("T")} \t Load Task : {taskIndex}");
+
+                lsTasks[i] = Task.Run(() => { return GenerateSameHashCodeString(new Random(), taskIndex); });
+            }
+
+            while (true)
+            {
+                Task.WaitAny(lsTasks);
+                Task<List<string>> completedTask = lsTasks.Where(x => x.IsCompleted && x.Result != null).FirstOrDefault();
+
+                String stringA = completedTask.Result[0];
+                String stringB = completedTask.Result[1];
+                String stringC = completedTask.Result[2];
+
+
+                if (stringA.GetHashCode() == stringB.GetHashCode() && !String.Equals(stringA, stringB) &&
+                    stringB.GetHashCode() == stringC.GetHashCode() && !String.Equals(stringB, stringC) &&
+                    !String.Equals(stringA, stringC)
+    )
+                {
+                    Console.WriteLine("Result : ");
+                    Console.WriteLine($"{stringA} with {stringA.GetHashCode()}");
+                    Console.WriteLine($"{stringB} with {stringB.GetHashCode()}");
+                    Console.WriteLine($"{stringC} with {stringC.GetHashCode()}");
+                    break;
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+
+        static List<string> GenerateSameHashCodeString(Random random, int index)
+        {
+            Console.WriteLine($"{DateTime.Now.ToString("T")} \t start Task : {index}");
+            while (true)
+            {
+                string tempString = string.Empty;
+
+                string generatedString = GenerateRandomString(random);
+                int hashCode = generatedString.GetHashCode();
+
+                lock (LsStringMap)
+                {
+                    if (LsStringMap == null)
+                    {
+                        Console.WriteLine($"{DateTime.Now.ToString("T")} \t Strop Task : {index}");
+                        return null;
+                    }
+
+                    if (LsStringMap.TryGetValue(hashCode, out tempString) && tempString != generatedString)
+                    {
+                        LsMatchHolder.Add(new StringHolder() { HashCode = hashCode, StringValue = tempString });
+                        LsMatchHolder.Add(new StringHolder() { HashCode = hashCode, StringValue = generatedString });
+
+                        var queryCheckMatch = LsMatchHolder.Where(x => x.HashCode == hashCode).Select(x => x.StringValue).Distinct();
+
+                        if (queryCheckMatch.Count() >= 3)
+                        {
+                            var stringRst = queryCheckMatch.Take(3).ToList();
+
+                            Console.WriteLine($"{DateTime.Now.ToString("T")} \t Task : {index} \t Match Found : {string.Join(" - ", stringRst)} with HashCode : {hashCode}");
+
+                            LsStringMap = null;
+
+                            Console.WriteLine($"{DateTime.Now.ToString("T")} \t Strop Task : {index}");
+
+                            return stringRst;
+                        }
+                    }
+
+                    LsStringMap[hashCode] = generatedString;
+                }
+            }
+        }
+
+        static string GenerateRandomString(Random random)
+        {
+            string generatedString = string.Empty;
+
+            for (int i = 0; i < 10; i++)
+            {
+                generatedString += (char)random.Next(char.MaxValue);
+            }
+            return generatedString;
+        }
+    }
+
+    class StringHolder
+    {
+        public string StringValue { get; set; }
+        public int HashCode { get; set; }
+    }
+}
